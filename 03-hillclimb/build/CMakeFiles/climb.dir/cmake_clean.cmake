ef922946fc3800c2ace213ee81dde8a2ed1a9a26
Isaiah Brown <ibrown@fandm.edu>
2022-10-25 11:49:11 -0400

    cmake added hw3

---
+++ 03-hillclimb/build/CMakeFiles/climb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/climb.dir/src/climb.cu.o"
+  "climb"
+  "climb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/climb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 03-hillclimb/build/CMakeFiles/rand.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rand.dir/src/rand.cu.o"
+  "rand"
+  "rand.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/rand.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
