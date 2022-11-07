3b4154083eb322b23f837191caac938112d9f8f6
Isaiah Brown <ibrown@fandm.edu>
2022-11-07 15:37:22 -0500

    new

--- 03-hillclimb/src/climb.cu
+++ 03-hillclimb/src/climb.cu
@@ -84,7 +84,7 @@ __global__ void local_max_kernel(float *map, int rows, int cols, int* bx, int* b
   float max_height = map[idx];
   bx[tid] = x;
   by[tid] = y;
-  
+
   for(int i = 0; i < steps; i++) {
     //float randNum = curand_uniform(&state[tid]);
     //int randIdx = int(4 * randNum);
@@ -152,6 +152,87 @@ __global__ void local_max_kernel(float *map, int rows, int cols, int* bx, int* b
 __global__ void local_max_restart_kernel(float *map, int rows, int cols, int* bx,
                                          int* by, int steps, curandState *state) {
   int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+  float start = curand_uniform(&state[tid]);
+  int idx = start * rows * cols;
+  int x = idx / cols;
+  int y = idx % cols;
+
+  float max_height = map[idx];
+  bx[tid] = x;
+  by[tid] = y;
+  
+  int last_max = 0;
+  for(int i = 0; i < steps; i++) {
+    //float randNum = curand_uniform(&state[tid]);
+    //int randIdx = int(4 * randNum);
+
+    float left, top, right, bottom;
+
+    
+    if (y != 0) {
+      int leftIdx = (x * 6114) + y - 1;
+      left = map[leftIdx];
+      //y = y - 1;
+    } else {
+      left = -99;
+    }
+
+    if (x != 0) {
+      int topIdx = ((x - 1) * 6114) + y;
+      top = map[topIdx];
+      //x = x - 1;
+    } else {
+      top = -99;
+    }
+       
+     
+    if (y != (cols -1)) {
+      int rightIdx = (x * 6114) + y + 1;
+      right = map[rightIdx];
+      //y = y + 1;
+      
+    } else {
+      right = -99;
+    }
+    
+    if (x != (rows - 1)) {
+      int bottomIdx = ((x + 1) * 6114) + y;
+      bottom = map[bottomIdx];
+      //x = x + 1;
+    } else {
+      bottom = -99;
+    }
+
+    if (left > top  && left > right && left > bottom) {
+      y = y - 1;
+    } else if (top > left && top > right && top > bottom) {
+      x = x - 1;
+    } else if (right > left && right > top && right > bottom) {
+      y = y + 1;
+    } else {
+      x = x + 1;
+    }
+
+    int currMaxIdx = x * 6114 + y;
+    float currMax = map[currMaxIdx];
+    last_max = last_max + 1;
+    
+    if (currMax > max_height) {
+      last_max = 0;
+      max_height = currMax;
+      bx[tid] = x;
+      by[tid] = y;
+    }
+
+    if (last_max > 4) {
+      float newStart = curand_uniform(&state[tid]);
+      int newIdx = newStart * rows * cols;
+      x = newIdx / cols;
+      y = newIdx % cols;
+    }
+
+  }
   //TODO: implement local max with restarts!
 }
 
@@ -258,7 +339,52 @@ float local_max(float* map, int rows, int cols, int steps){
   free(by);
   return max_val;
 }
-float local_max_restart(float* map, int rows, int cols, int steps);
+float local_max_restart(float* map, int rows, int cols, int steps) {
+  curandState* d_state = init_rand();
+  int *bx, *by;
+  int *d_bx, *d_by;
+  float* d_map;
+
+  // Before kernel call:
+  // Need to allocate memory for above variables and copy data to GPU
+
+  bx = (int*)malloc(N_BLOCKS * N_THREADS * sizeof(float));
+  by = (int*)malloc(N_BLOCKS * N_THREADS * sizeof(float));
+  cudaMalloc(&d_bx, N_BLOCKS * N_THREADS * sizeof(float));
+  cudaMalloc(&d_by, N_BLOCKS * N_THREADS * sizeof(float));
+
+  
+  cudaMalloc(&d_map, rows * cols * sizeof(float));
+  cudaMemcpy(d_map, map, rows * cols * sizeof(float), cudaMemcpyHostToDevice);
+
+  local_max_restart_kernel<<<N_BLOCKS, N_THREADS>>>(d_map, rows, cols, d_bx, d_by, steps, d_state);
+
+  cudaMemcpy(map, d_map, N_BLOCKS * N_THREADS * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(bx, d_bx, N_BLOCKS * N_THREADS * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(by, d_by, N_BLOCKS * N_THREADS * sizeof(float), cudaMemcpyDeviceToHost);
+
+  // After kernel call:
+  // Need to copy data back to CPU and find max value
+  float max_val = 0;
+  for(int i = 0; i < N_BLOCKS * N_THREADS; i++) {
+    int x = bx[i];
+    int y = by[i];
+    int idx = x * 6114 + y;
+    float height = map[idx];
+    if (height > max_val) {
+      max_val = height;
+    }
+  }
+
+  // Finally: free used GPU and CPU memory
+  cudaFree(d_state);
+  cudaFree(d_map);
+  cudaFree(d_bx);
+  cudaFree(d_by);
+  free(bx);
+  free(by);
+  return max_val;
+}
 
 
 int main(int argc, char** argv) {
@@ -276,9 +402,9 @@ int main(int argc, char** argv) {
   // As a starting point, try to get it working with a single steps value
   int steps = 1;
   while(steps <= 1024) {
-    float max_val = local_max(map, rows, cols, steps);
+    float max_val = local_max_restart(map, rows, cols, steps);
     //printf("%d %d\n", rows, cols);
-    printf("Local Max steps: %d, max value: %f\n", steps, max_val);
+    printf("with reset: %d, max value: %f\n", steps, max_val);
     steps = steps * 2;
   }
   free(map);
